20021d5c83c4771b47723552ece771a59087547e
Vitor Hugo Risso da Silva <CIANDT\vitorhr@lnb028042cps.ciandt.global>
2021-03-25 18:25:44 -0300

    aula 2 finalizada

--- src/main/kotlin/br/com/example/myproject/main.kt
+++ src/main/kotlin/br/com/example/myproject/main.kt
@@ -1,23 +1,37 @@
 package br.com.example.myproject
 
-import br.com.example.myproject.model.Address
-
 fun main() {
     println("STARTING... \n")
+
+    val entrada = "1"
+    val value: Int? = try {
+        entrada.toInt()
+    } catch (e: NumberFormatException) {
+        e.printStackTrace()
+        null
+    }
+    if(value != null){
+        println("Valor recebudo $value")
+    }
+
+    val valueWithTax = if(value!= null){
+        value + 32
+    } else
+        null
 
     funcao1()
 
     println("Stop")
 }
 
-fun funcao1(){
+fun funcao1() {
     println("Função 1 inicio")
     funcao2()
     println("Função 1 fim")
 }
 
-fun funcao2(){
-     println("Função 2 inicio")
+fun funcao2() {
+    println("Função 2 inicio")
     println("Função 2 fim")
 }
 
