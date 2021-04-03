66e48a68f614c71aaf1bb3d3b996b833f53443b5
Vitor Hugo Risso da Silva <CIANDT\vitorhr@lnb028042cps.ciandt.global>
2021-04-03 13:07:09 -0300

    aula 4 finalizada

--- src/main/kotlin/br/com/example/myproject/main.kt
+++ src/main/kotlin/br/com/example/myproject/main.kt
@@ -3,13 +3,37 @@ package br.com.example.myproject
 fun main() {
     println("STARTING... \n")
 
-    val pedidos = mutableMapOf(Pair(1, 20.0), 2 to 34.0)
+    val pedidos = mutableMapOf(
+        Pair(1, 20.0),
+        2 to 34.0,
+    )
+
+//    val velorPedido = pedidos.getValue(5)
+
+    pedidos.getOrElse(9, {
+        "Pedido não cadastrado"
+    })
+    pedidos.getOrDefault(0, -1.0) // devolve vaor default
+    pedidos.keys // devolve as chaves
+    pedidos.values // devolve os valores
+    pedidos.filter { (number, value) ->
+        number % 2 == 0
+    } // devolve um novo array
+    pedidos.filterKeys { key ->
+        key % 2 ==0
+    }
+    pedidos.filterValues { value ->
+        value > 50.0
+    }
+}
+
+private fun mapTest(pedidos: MutableMap<Int, Double>) {
     val pedido = pedidos[1] // precisa colocar a chave nao a posicao do item
     pedido?.let {
         println(pedido)
     }
 
-    for(entry in pedidos){
+    for (entry in pedidos) {
         println(entry.key)
         println(entry.value)
     }
